2e06b220b73c76cbc523a4a749ab2941eb7b8e45
Jakub Grzesik <kubecz3k@gmail.com>
2016-06-02 11:23:54 +0200

    Update android part of the module to run with Godot 2.0

--- flurry/config.py
+++ flurry/config.py
@@ -6,16 +6,18 @@ def configure(env):
 
 	import string
 	if env['platform'] == "playbook":
-
 		env.Append(LIBPATH=['#modules/flurry/bps/Flurry/$qnx_target_ver', '$QNX_TARGET/$qnx_target_ver/usr/lib/qt4/lib'])
 		env.Append(LIBS=string.split("cpp QtCore curl crypto packageinfo bbdevice")) # holy shit
 		env.Append(LIBS=['Flurry'])
 
-	elif env['platform'] == "iphone": # iphone
+	elif env['platform'] == "iphone":
 		env.Append(LIBPATH=['#modules/flurry/ios'])
 		env.Append(LIBS=['Flurry'])
 
 	elif env['platform'] == 'android':
-		env.android_module_library("android/FlurryAgent.jar")
-		env.android_module_file("android/GodotFlurry.java")
+		env.android_add_java_dir("android/src")
+		env.android_add_dependency("compile files('../../../modules/flurry/android//FlurryAgent.jar')")
+#		env.android_add_dependency("compile 'com.google.android.gms:play-services:7.3.+'")
 		env.disable_module()
+
+
